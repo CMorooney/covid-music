29e6f97fac614745ed9c781b113ecfbc2df29056
Calvin <calvin@Calvins-MacBook-Pro.local>
2020-03-18 18:33:31 -0400

    adds fminor scale and maps from rGroup to notes in array.

--- generate.py
+++ generate.py
@@ -9,16 +9,20 @@ P_ORF7a = "data/ORF7a_protein_QIB84678_1"
 P_ORF8 = "data/ORF8_protein_QIB84679_1"
 P_ENVELOPE = "data/envelope_protein_QIB84675_1"
 P_MEMBRANE_GLYCO = "data/membrane_glycoprotein_QIB84676_1"
-P_NUCLEOCAPSID_PHOSPHO = "nucleocapsid_phosphoprotein_QIB84680_1"
+P_NUCLEOCAPSID_PHOSPHO = "data/nucleocapsid_phosphoprotein_QIB84680_1"
 P_ORFLAB = "data/orflab_polyprotein_QIB84672_1"
 P_SURFACE_GLYCO = "data/surface_glycoprotein_QIB84673_1"
 
-degrees  = [60, 62, 64, 65, 67, 69, 71, 72] # MIDI note number
+#middle-c to b
+notes = { 'c':60, 'c#':61, 'd':62, 'd#':63, 'e':64, 'f':65, 'f#':66, 'g':67, 'g#':68, 'a':69, 'a#':70, 'b':71 }
+rGroups = ['A', 'C', 'D', 'E', 'F','G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+fMinor = [notes['f'], notes['g'], notes['g#'], notes['a#'], notes['c'], notes['c#'], notes['d#']]
+
 track    = 0
 channel  = 0
 time     = 0   # In beats
-duration = 1   # In beats
-tempo    = 60  # In BPM
+duration = .5   # In quarter beats
+tempo    = 145  # In BPM
 volume   = 100 # 0-127, as per the MIDI standard
 
 MyMIDI = MIDIFile(1) # One track, defaults to format 1 (tempo track
@@ -29,20 +33,32 @@ def parseProtein(proteinFilePath):
   with open(proteinFilePath) as f:
     while True:
       c = f.read(1)
-      if not c:
+      if not c or c.isspace():
         print "End of file"
+        writeFile()
         break
       parseCharacter(c)
 
 def parseCharacter(character):
-  ##TODO
-  print "Parse: ", character
+  writeNote(character, fMinor)
 
-parseProtein(P_ORF_10)
+def writeNote(rGroup, scale):
+  index = rGroups.index(rGroup)
+  note = 0
+  scaleLength = len(scale)
 
-#for pitch in degrees:
-#    MyMIDI.addNote(track, channel, pitch, time, duration, volume)
-#    time = time + 1
-#
-#with open("test.mid", "wb") as output_file:
-#    MyMIDI.writeFile(output_file)
+  if(index < scaleLength):
+   note = fMinor[index]
+  else:
+    note = fMinor[index % scaleLength]
+
+  MyMIDI.addNote(track, channel, note, time, duration, volume) 
+
+  global time
+  time = time + .5 
+
+def writeFile():
+  with open("test.mid", "wb") as output_file:
+   MyMIDI.writeFile(output_file)
+
+parseProtein(P_NUCLEOCAPSID_PHOSPHO)
